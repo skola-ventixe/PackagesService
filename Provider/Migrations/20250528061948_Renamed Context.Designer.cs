89bf90468503889b879b4c0802f1d69439021a95
Tobias Vinther <Tobias.Vinther88@gmail.com>
2025-05-28 08:28:07 +0200

    Changed Context Name

--- Provider/Migrations/20250528061948_Renamed Context.Designer.cs
+++ Provider/Migrations/20250528061948_Renamed Context.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Provider.Data;
 
@@ -10,9 +11,11 @@ using Provider.Data;
 namespace Provider.Migrations
 {
     [DbContext(typeof(PackagesDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250528061948_Renamed Context")]
+    partial class RenamedContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
