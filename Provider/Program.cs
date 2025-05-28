997af9bd8a6afb6ffd99e697f906d5c681406735
Tobias Vinther <Tobias.Vinther88@gmail.com>
2025-05-28 08:44:22 +0200

    reordered a bit in program

--- Provider/Program.cs
+++ Provider/Program.cs
@@ -5,30 +5,39 @@ using Provider.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<PackagesDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 
 var serviceBusConnection = builder.Configuration["ServiceBusConnection"];
 builder.Services.AddSingleton(x => new ServiceBusClient(serviceBusConnection));
 builder.Services.AddSingleton(x =>
     x.GetRequiredService<ServiceBusClient>().CreateSender("event-bus"));
+builder.Services.AddSingleton<PackageBusListener>();
 builder.Services.AddHostedService<PackageBusListener>();
-
 
 builder.Services.AddScoped<IPackageService, PackageService>();
 
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<PackagesDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
-app.UseCors(policy =>
-    policy.AllowAnyOrigin()
-          .AllowAnyMethod()
-          .AllowAnyHeader());
-
 app.MapOpenApi();
 app.UseHttpsRedirection();
+app.UseCors("AllowAllOrigins");
 
 app.UseAuthorization();
 
