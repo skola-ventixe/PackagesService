ba46ded780db13ae3e7b8828904474c75ede79f6
Tobias Vinther <Tobias.Vinther88@gmail.com>
2025-06-05 08:28:46 +0200

    Changed the way i get the Vault uri

--- Provider/Program.cs
+++ Provider/Program.cs
@@ -7,7 +7,7 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri")!);
+var keyVaultEndpoint = new Uri(builder.Configuration["VaultUri"]!);
 builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 builder.Services.AddControllers();
